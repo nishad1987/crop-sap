194a91adeb11727f0e3aab489ff1f93ae8154ee6
nishad1987 <nishad.chitrasen@gmail.com>
2023-10-30 18:07:38 +0530

    frontend file added

---
+++ frontend/src/components/navbar/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+const NavBar = () => (
+    <nav>
+        <div className="nav-wrapper grey darken-4">
+        <ul>
+            <li><a href="/" className="brand-logo left">Home</a> </li>
+            <li> <a href="/" className="brand-logo center">Add User</a> </li>
+            <li> <a href="/" className="brand-logo right">list User</a>  </li>
+        </ul>
+        
+          
+             
+        </div>
+    </nav>
+);
+
+export default NavBar;
---
+++ frontend/src/components/navebar/index.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const NavBar = () => (
+         <nav>
+            <div className="nav-wrapper grey darken-4">
+               <a href="/" className="brand-logo center">Home</a>
+            </div>
+         </nav>
+
+);
+
+export default NavBar;
